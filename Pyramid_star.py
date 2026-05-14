c666fd6025ab21a7229828bd1245703842e28cab
ece <avcoe823@gmail.com>
2026-05-14 19:48:52 +0330

    Pyramid_star.py

---
+++ Pyramid_star.py
@@ -0,0 +1,8 @@
+n =int(input("Enter a rows as you want:"))
+
+for i in range(1,n+1):
+    for space in range(n-i):
+        print(" ",end='')
+    for star in range (2*i-1):
+        print("*",end='')
+    print ()
